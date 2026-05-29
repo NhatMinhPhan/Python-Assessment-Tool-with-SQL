b82ff64005b808d351c0083f4e6b96387ae33298
NhatMinhPhan <nhatminh120767@gmail.com>
2026-05-29 03:23:22 -0400

    Edited authsql.py

--- flask/flaskapp/authsql.py
+++ flask/flaskapp/authsql.py
@@ -84,7 +84,7 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-            'SELECT * FROM ACCOUNT WHERE id = ?', (user_id,)
+            'SELECT * FROM ACCOUNT WHERE AccountID = ?', (user_id,)
         )
 
 @cross_origin
